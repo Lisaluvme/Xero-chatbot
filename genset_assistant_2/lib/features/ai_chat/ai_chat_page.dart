14c7d73db2418ba1a3f9a74de4b487de8496d81f
swatchie-1 <swatchie-1@github.com>
2025-10-09 10:26:36 +0800

    Update Play Store signing configuration: new keystore, build fixes, and dependency updates

--- genset_assistant_2/lib/features/ai_chat/ai_chat_page.dart
+++ genset_assistant_2/lib/features/ai_chat/ai_chat_page.dart
@@ -6,7 +6,7 @@ import 'package:share_plus/share_plus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
-import 'package:printing/printing.dart';
+// import 'package:printing/printing.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'dart:io';
 import '../../services/knowledge_base_service.dart';
